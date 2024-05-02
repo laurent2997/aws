12784bdcae92382014105712a9a37254be23b4b6
laurent2997 <162601315+laurent2997@users.noreply.github.com>
2024-05-02 17:14:32 +0200

    add wind and snow colors

--- colors.js
+++ colors.js
@@ -13,7 +13,26 @@ const COLORS = {
         {min: 20, max: 25, color: "#ff5a41" },
         {min: 25, max: 30, color: "#ff1f24" },
         {min: 30, max: 50, color: "#fa3c96" },
-        
 
+    ],
+    wind: [
+        {min: 0, max: 5, color: "#ffff64" },
+        {min: 5, max: 10, color: "#c8ff64" },
+        {min: 10, max: 20, color: "#96ff96" },
+        {min: 20, max: 40, color: "#32c8ff" },
+        {min: 40, max: 60, color: "#6496ff" },
+        {min: 60, max: 80, color: "#9664ff" },
+        {min: 80, max: 250, color: "#ff3232" },
+    ],
+    snow: [
+        {min: 1, max: 10, color: "#ffffb2" },
+        {min: 10, max: 25, color: "#b0ffbc" },
+        {min: 25, max: 50, color: "#8cffff" },
+        {min: 50, max: 100, color: "#19cdff" },
+        {min: 100, max: 200, color: "#1982ff" },
+        {min: 200, max: 300, color: "#0f5abe" },
+        {min: 300, max: 400, color: "#784bff" },
+        {min: 400, max: 1000, color: "#cd0feb" },
     ]
+
 };
